4c79a483b59111fe5534f2a167a3d16ddc7bc245
Carlos <carlosfls54@gmail.com>
2025-12-11 20:48:52 -0300

    Initial commit

---
+++ src/main/java/org/carlosacademic/springbootandspringmvc/SpringBootAndSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.carlosacademic.springbootandspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAndSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/carlosacademic/springbootandspringmvc/SpringBootAndSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosacademic.springbootandspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
